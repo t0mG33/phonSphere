34a2d52c434407077467de98e7e54d8d702c7bb8
t0mG33 <tommygalmiche@outlook.com>
2023-12-05 20:53:49 -0500

    Created "scr_specs_json-to-db.php"

---
+++ scr_specs_json-to-db.php
@@ -0,0 +1,94 @@
+<?php 
+    
+    $JSONdevicesArr = file_get_contents("devices.json");
+    $devicesDecode = json_decode($JSONdevicesArr, TRUE);
+    
+    $typeLength = 0;
+    $clrRgeLen = 0;
+    $touchLen = 0;
+    $sizeLen = 0;
+    $resLen = 0;
+    $ratioLen = 0;
+
+
+    include 'dbhandler.php';
+    
+    if ($mysqli->connect_error) {
+        echo 'Errno: '.$mysqli->connect_errno;
+        echo '<br>';
+        echo 'Error: '.$mysqli->connect_error;
+        exit();
+    }
+
+    echo 'Success: A proper connection to MySQL was made.';
+
+    foreach ($devicesDecode as $key => $jsons) { // This will search in the 2 jsons
+
+        foreach($jsons as $key => $spec) {
+
+            if ($key === '_screen') {
+                foreach ($spec as $scrKey=>$scrSpec) {
+                    switch ($scrKey) {
+                        case 'type':
+                            $scrType = $scrSpec;
+                            // if (strlen($scrSpec) > $typeLength) {
+                            //     $typeLength = strlen($scrSpec);
+                            // }
+                            break;
+
+                        case 'colorRge':
+                            $scrColRge = $scrSpec;
+                            // if (strlen($scrSpec) > $clrRgeLen) {
+                            //     $clrRgeLen = strlen($scrSpec);
+                            // }
+                            break;
+
+                        case 'touchscreen':
+                            $touchScreen = $scrSpec;
+                            // if (strlen($scrSpec) > $touchLen) {
+                            //     $touchLen = strlen($scrSpec);
+                            // }
+                            break;
+
+                        case 'size':
+                            $scrSize = $scrSpec;
+                            // if (strlen($scrSpec) > $sizeLen) {
+                            //     $sizeLen = strlen($scrSpec);
+                            // }
+                            break;
+
+                        case 'res':
+                            $scrRes = $scrSpec;
+                            // if (strlen($scrSpec) > $resLen) {
+                            //     $resLen = strlen($scrSpec);
+                            // }
+                            break;
+
+                        case 'ratio':
+                            $scrRatio = $scrSpec;
+                            // if (strlen($scrSpec) > $ratioLen) {
+                            //     $ratioLen = strlen($scrSpec);
+                            // }
+                            break;
+                    }
+                }
+            }
+        }
+
+
+        $sql = "INSERT INTO scr_specs (scr_type, clr_rge, touch_scr, scr_size, scr_res, scr_ratio)
+                VALUES ('$scrType', '$scrColRge', '$touchScreen', $scrSize,  '$scrRes', '$scrRatio')";
+
+        $mysqli->query($sql);
+        echo mysqli_error($mysqli);
+   }
+
+//    echo 'Type string is ' . $typeLength . ' characters', "\n";
+//    echo 'ColorRange string is ' . $clrRgeLen . ' characters', "\n";
+//    echo 'Touchscreen string is ' . $touchLen . ' characters', "\n";
+//    echo 'ScreenSize string is ' . $sizeLen . ' characters', "\n";
+//    echo 'ScrRes string is ' . $resLen . ' characters', "\n";
+//    echo 'ScrRatio string is ' .  $ratioLen . ' characters', "\n";
+
+    $mysqli->close();
+?>
