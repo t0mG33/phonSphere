40f35f7af5728dfb8c673e141157dda2a587b91b
t0mG33 <tommygalmiche@outlook.com>
2024-04-06 17:21:39 -0400

    Refactored queries

--- devices.php
+++ devices.php
@@ -12,13 +12,14 @@
     $phpScreenSize = null;
     $phpBattCap = null;
     //Recovering JSON data for devices
-    $JSONdevicesArr = file_get_contents("devices.json");
-    $devicesDecode = json_decode($JSONdevicesArr, TRUE);
+    // $JSONdevicesArr = file_get_contents("devices.json");
+    // $devicesDecode = json_decode($JSONdevicesArr, TRUE);
     //Scope variable to store selection of devices
     $selectArr = [];
-    $clientIp = $_SERVER['REMOTE_ADDR'];
-    $clientDate = date("Y-m-d");
-    $clientTime = date("h:i:sa");
+    // $clientIp = $_SERVER['REMOTE_ADDR'];
+    // $clientDate = date("Y-m-d");
+    // $clientTime = date("h:i:sa");
+    global $sql;
 
     function setParams() {
 
@@ -84,72 +85,78 @@
 
     setParams();
 
-    //Call database and store user's inputs (IP address + options selection)
-    //Resource: https://www.codexworld.com/how-to/get-user-ip-address-php/
     include 'dbhandler.php'; //Create connection
 
     // FOR DEBUGGING PURPOSES ONLY: Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    // FOR DEBUGGING PURPOSES ONLY:
-    // echo "Connected successfully";
-    $sql = "INSERT INTO response (log_time, log_address, key_feature, scr_size, batt_cap)
-    VALUES ('$clientDate.$clientTime', '$clientIp', '$phpKeyFeat', '$phpScreenSize', '$phpBattCap')";
-    
-    $conn->query($sql);
-    // $conn->close();
-    
-    // FOR DEBUGGING PURPOSES ONLY:
-    // if ($conn->query($sql) === TRUE) {
-    //     echo "New record created successfully";
-    // } else {
-    //     echo "Error: " . $sql . "<br>" . $conn->error;
-    // }
 
-    $conn->close();
+    switch ($phpKeyFeat) {
+        case "camera":
 
-    foreach ($devicesDecode as $item) {
+            $sql = "SELECT *
+                    FROM primary_cam_specs
+                    INNER JOIN battery_specs ON battery_specs.id = primary_cam_specs.id
+                    INNER JOIN core_specs ON core_specs.id = battery_specs.id
+                    INNER JOIN scr_specs ON scr_specs.id = core_specs.id
+                    INNER JOIN devices ON devices.id = scr_specs.id
+                    WHERE primary_cam_specs.camRes >= 8 OR primary_cam_specs.lensWidth < 29 OR primary_cam_specs.OIS = 1 OR primary_cam_specs.opticZoom = 1 OR primary_cam_specs.hdr = 1
+                    OR core_specs.min_builtIn_str >= 32 OR core_specs.ext_str_cap >= 64";
+            
+            break;
 
-        switch ($phpKeyFeat) {
-            case "camera":
+        case "screen":
 
-                if ($item['_mainCamFeats']['camRes'] >= 8 || $item['_mainCamFeats']['camExp']['lenseWidth'] < 29 || $item['_mainCamFeats']['ois'] || $item['_mainCamFeats']['opticalZoom'] || $item['_mainCamFeats']['hdr'] || $item['_storage']['builtIn'][0] >= 32 && $item['_storage']['ext'] == true) {
-                    array_push($selectArr, $item);
-                    // if ($item['_name'] === "KEYone") {
-                    //     echo "BLACKBERRY KEYONE!!!";
-                    // }
-                }
-                
-                break;
-    
-            case "screen":
-                //Push in screens using AMOLED technology AND 2K resolution (1920x1080 pixels)
-                //Reference: https://www.quora.com/What-is-the-best-display-of-a-smartphone
-                if ($item['_screen']['type'] == 'AMOLED' || $item['_screen']['type'] == 'Super AMOLED' || $item['_screen']['type'] == 'P-OLED' && $item['_screen']['res'] == '1080 x 1920' || $item['_screen']['res'] == '1080 x 2340' ||$item['_screen']['res'] == '1440 x 2960' || $item['_screen']['res'] == '1440 x 3120' ) {
-                    array_push($selectArr, $item);
-                }
-                break;
+            $sql = "SELECT *
+                    FROM scr_specs
+                    INNER JOIN battery_specs ON battery_specs.id = scr_specs.id
+                    INNER JOIN core_specs ON core_specs.id = battery_specs.id
+                    INNER JOIN devices ON devices.id = core_specs.id
+                    WHERE scr_specs.scr_type = 'AMOLED' OR scr_specs.scr_type = 'Super AMOLED' OR scr_specs.scr_type = 'P-OLED'
+                    AND scr_specs.scr_res = '1080 x 1920' OR scr_specs.scr_res = '1080 x 2340' OR scr_specs.scr_res = '1440 x 2960' OR scr_specs.scr_res = '1440 x 3120'";
 
-            case "basic":
-                //Check that "_gps" array contains the values "A-GPS" AND "GLONASS" AND has an basic LCD display                
-                if (in_array("A-GPS", $item['_gps']) && in_array("GLONASS", $item['_gps']) && $item['_screen']['type'] === "IPS LCD" || $item['_screen']['type'] === "LED-backlit IPS LCD") {
-                    array_push($selectArr, $item);
-                }
-                break;
-        };
+            break;
+
+        case "basic":
+            
+            //Check that "_gps" array contains the values "A-GPS" AND "GLONASS" AND has an basic LCD display                
+            $sql = "SELECT *
+                    FROM gps_specs
+                    INNER JOIN battery_specs ON battery_specs.id = gps_specs.id
+                    INNER JOIN core_specs ON core_specs.id = battery_specs.id
+                    INNER JOIN scr_specs ON scr_specs.id = core_specs.id
+                    INNER JOIN devices ON devices.id = scr_specs.id
+                    WHERE gps_specs.has_a_gps = 1 AND gps_specs.has_glonass = 1 AND scr_specs.scr_type = 'IPS LCD' OR scr_specs.scr_type = 'LED-backlit IPS LCD'";
+            
+            break;
     };
 
+    //Querying DB via MySQL
+    $result = $conn->query($sql);
+    //Create array with SQL outputs
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            array_push($selectArr, $row);
+        }
+    } else {
+        echo "0 results";
+    }
+
+    //Closing MySQL connexion
+    $conn->close();
+
+    //Refining SQL results array
     foreach ($selectArr as $device) {
 
         switch ($phpScreenSize) {
             case "small":
                 if ($phpBattCap != 'high') {
-                    if ($device['_screen']['size'] > 5.0) {
+                    if ($device['scr_size'] > 5.0) {
                         array_splice($selectArr, array_search($device, $selectArr),1);
                     }
                 } else {
-                    if ($device['_screen']['size'] > 5.9) {
+                    if ($device['scr_size'] > 5.9) {
                         array_splice($selectArr, array_search($device, $selectArr),1);
                     }
                 }
@@ -157,7 +164,7 @@
                 break;
 
             case "large":
-                if ($device['_screen']['size'] < 5.0) {
+                if ($device['scr_size'] < 5.0) {
                     array_splice($selectArr, array_search($device, $selectArr),1);
                 }
                 break;
@@ -169,55 +176,53 @@
         //Battery technology: http://blog.ravpower.com/2017/06/lithium-ion-vs-lithium-polymer-batteries/
         //"Best battery life phones 2019": https://www.techadvisor.co.uk/test-centre/mobile-phone/best-battery-life-phone-3679230/
         // Li-Ion batteries has more power than Li-Po but are heavier: https://www.quora.com/Which-type-of-battery-for-smartphone-is-better-Li-Ion-or-Li-Po
-            if ($phpBattCap == 'high') {
+        if ($phpBattCap == 'high') {
             //Restrain to mAh battery capacity above 3000
-            if ($device['_devBattery']['mAhCapacity'] < 3000) {
-                // if ($device['_name'] === "KEYone") {
-                //     echo "BLACKBERRY KEYONE!!!";
-                // }
-                // echo 'Battery cap. below 3000';
+            if ($device['mAh_cap'] < 3000) {
                 array_splice($selectArr, array_search($device, $selectArr),1);
             }
         };
-        // echo 'Filtered out battery cap., selected Array counts: '.count($selectArr).'</br>';
     };
 
+    //Sorting SQL array according to criteria
+    //Sorting according camera features
     if ($phpKeyFeat == 'camera') {
 
         function sortDevices($a,$b)
         {
-        if ($a['_mainCamFeats']['camRes'] == $b['_mainCamFeats']['camRes']) return 0;
-        return ($b['_mainCamFeats']['camRes'] < $a['_mainCamFeats']['camRes'])?-1:1;
-        };
-
-        usort($selectArr,"sortDevices");
-
-    } else if ($phpScreenSize == 'small' || $phpScreenSize == 'large') {
-
-        function sortDevices($a,$b)
-        {
-        if ($a['_screen']['size'] == $b['_screen']['size']) return 0;
-        return ($b['_screen']['size'] < $a['_screen']['size'])?-1:1;
+            if ($a['camRes'] == $b['camRes']) return 0;
+            return ($b['camRes'] < $a['camRes'])?-1:1;
         };
 
         usort($selectArr,"sortDevices");
 
     }
-    // else if ($phpBattCap == 'high') {
-    //     function sortDevices($a,$b)
-    //     {
-    //     if ($a['_devBattery']['mAhCapacity'] == $b['_devBattery']['mAhCapacity']) return 0;
-    //     return ($b['_devBattery']['mAhCapacity'] < $a['_devBattery']['mAhCapacity'])?-1:1;
-    //     };
+    //Sorting according to screen features
+    else if ($phpScreenSize == 'small' || $phpScreenSize == 'large') {
 
-    //     usort($selectArr,"sortDevices");
-    // }
+        function sortDevices($a,$b)
+        {
+            if ($a['scr_size'] == $b['scr_size']) return 0;
+            return ($b['scr_size'] < $a['scr_size'])?-1:1;
+        };
 
+        usort($selectArr,"sortDevices");
+
+    }
+    //Sorting according to battery features
+    else if ($phpBattCap == 'high') {
+        function sortDevices($a,$b)
+        {
+            if ($a['mAh_cap'] == $b['mAh_cap']) return 0;
+            return ($b['mAh_cap'] < $a['mAh_cap'])?-1:1;
+        };
+
+        usort($selectArr,"sortDevices");
+    }
+
+    //Retaining top 3 for selection array
     $selectArr = array_splice($selectArr, 0, 3);
 
-    // foreach ($selectArr as $phone) {
-    //     echo $phone['_name'];
-    // }
 ?>
 <!DOCTYPE html>
 <html class="no-js" lang="">
@@ -233,19 +238,11 @@
   <!-- Place favicon.ico in the root directory -->
 
   <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="/css/main.css">
+  <link rel="stylesheet" href="css/main.css">
 
   <style>
     body {background: floralwhite}
   </style>
-
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({
-          google_ad_client: "ca-pub-9573519566042715",
-          enable_page_level_ads: true
-     });
-</script>
   
 </head>
 
@@ -260,14 +257,9 @@
         <?php foreach ($selectArr as $item) :?>
 
             <div class="dev-card">
-                <img src="<?= $item['imgUrl'] ?>" alt="<?= $item['_brand'] + $item['_name'] ?>">
+                <img src="<?= $item['image'] ?>" alt="<?= $item['brand'] . ' ' . $item['name'] ?>"/>
                 <div class="caption">
-                    <h5><?= $item['_brand'] . " " . $item['_name'] ?></h5>
-
-                    <? if ($item['amaffilink']): ?>
-                        <a href="<?= $item['amaffilink'] ?>" class="affil-btn" target="_blank">Buy on Amazon!</a>
-                    <? endif; ?>
-
+                    <h5><?= $item['brand'] . " " . $item['name'] ?></h5>
                 </div>
                 
             </div>
@@ -276,22 +268,11 @@
 
     </div>
 
-    <a href="/">
+    <a href="index.php">
         <button id="startAgainBtn" class="custom-btn">Start again</button>
     </a>
 
     <?php include 'includes/footer.php';?>
-
-    <script>
-
-        var captionDiv = document.getElementsByClassName("caption");
-        for (var i=0; i<captionDiv.length; i++) {
-            if (captionDiv[i].querySelector(".affil-btn")) {
-                captionDiv[i].classList.add("expanded")
-            }
-        }
-
-    </script>
 
 </body>
 </html>
