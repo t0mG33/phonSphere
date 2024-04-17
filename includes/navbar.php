300a5e84124d2579784e7ccff0ab1609bb4b7e12
t0mG33 <tommygalmiche@outlook.com>
2024-04-16 22:03:22 -0400

    Added Bootstrap grid on navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,11 @@
-<div class="navbar">
-    <a id="logo" href="#">
-        <img src="img/logo.png" alt="">
-    </a>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="navbar">
+                <a id="logo" href="#">
+                    <img src="img/logo.png" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
