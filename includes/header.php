9a1281590362dd5ef61539656ec8129e98fd80c4
t0mG33 <tommygalmiche@outlook.com>
2024-04-16 22:02:49 -0400

    Updated styles for header intro and added start button

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,8 @@
             <h1>Your new cellphone with just 3 questions</h1>
             <h2>No tech specs...</h2>
             <h2>No upselling...</h2>
-            <h2>No hassle in store...</h2>
+            <h2>No hassle in store.</h2>
+            <button id="start-btn">Start</button>
         </div>
     </div>
 </header>
