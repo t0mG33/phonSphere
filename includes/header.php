0d82da9243e52163a16e91f8a97970ff6ae6962b
t0mG33 <tommygalmiche@outlook.com>
2024-04-16 22:04:30 -0400

    Added Bootstrap grid on header

--- includes/header.php
+++ includes/header.php
@@ -2,13 +2,19 @@
 
     <?php include 'includes/navbar.php';?>
 
-    <div class="jumbo">
-        <div class="txt-ctt">
-            <h1>Your new cellphone with just 3 questions</h1>
-            <h2>No tech specs...</h2>
-            <h2>No upselling...</h2>
-            <h2>No hassle in store.</h2>
-            <button id="start-btn">Start</button>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="jumbo">
+                    <div class="txt-ctt">
+                        <h1>Your new cellphone with just 3 questions</h1>
+                        <h2>No tech specs...</h2>
+                        <h2>No upselling...</h2>
+                        <h2>No hassle in store.</h2>
+                        <button id="start-btn">Start</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </header>
